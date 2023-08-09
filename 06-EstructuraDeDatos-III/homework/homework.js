30aa5b61e64f225917208c31185de8bbf8b25fc1
DanielRS957 <drsp9501@icloud.com>
2023-08-08 21:38:12 -0500

    practicando codigo

--- 06-EstructuraDeDatos-III/homework/homework.js
+++ 06-EstructuraDeDatos-III/homework/homework.js
@@ -27,58 +27,65 @@ BinarySearchTree.prototype.insert = function(value){
    if(value < this.root){
       if(this.left){
          this.left.insert(value)
-      }
-      this.left = new BinarySearchTree(value);
-      // return this.left
+      }else{
+         this.left = new BinarySearchTree(value);
+      }  
    }
-   else{
-      if(value>this.root){
-         if(this.right){
-            this.right.insert(value);
-         }
+   else {
+      if(this.right){
+         this.right.insert(value);
+      }else{
          this.right = new BinarySearchTree(value);
-         // return this.right
       }
    }
-   // this._length++;
 };
-
+// let nueva = new BinarySearchTree(9);
+// nueva.insert(11);
+// nueva.insert(5)
+// nueva.insert(10)
+// console.log(nueva);
 // - contains: retorna true o false luego de evaluar si cierto valor existe dentro del árbol
 
 BinarySearchTree.prototype.contains = function(value){
-   let current = this.root;
-   let resultado;
-   if(current){
-      if(this.left){
-         this.left.contains(value);
-      }
-      else if(current === value){
-         return true;
-      }
-      this.left.contains(value);
+   if (this.root === value) {
+      return true;
+   } else if (value < this.root && this.left !== null) {
+      return this.left.contains(value);
+   } else if (value > this.root && this.right !== null) {
+      return this.right.contains(value);
+  } else {
+     return false;
    }
-   else if(this.right){
-         this.right.contains(value);
-      }
-   resultado= current===value;
-   
 };
-let nueva = new BinarySearchTree(9);
-nueva.insert(11);
-nueva.insert(5);
-// nueva.insert()
-console.log(nueva);
-console.log(nueva.size());
-nueva.contains(5);
-
-
-
+// console.log(nueva.contains(5));
 // - depthFirstForEach: recorre el árbol siguiendo el orden depth first (DFS) en cualquiera de sus variantes,
 // según se indique por parámetro ("post-order", "pre-order", o "in-order"). Nota: si no se provee ningún parámetro,
 //  hará el recorrido "in-order" por defecto.
 
-BinarySearchTree.prototype.depthFirstForEach = function(){
+BinarySearchTree.prototype.depthFirstForEach = function(cb,pedido){
+   if(pedido === "in-order" || !pedido){
+      if(this.left && this.left.depthFirstForEach(cb,pedido));
+      cb(this.root);
+      if(this.right && this.right.depthFirstForEach(cb,pedido));
+   }
+   if(pedido=== "pre-order"){
+     cb(this.root);
+      if(this.left && this.left.depthFirstForEach(cb,pedido));
+      if(this.right && this.right.depthFirstForEach(pedido));
+   }
+   if(pedido=== "post-order"){
+      if(this.left && this.left.depthFirstForEach(cb,pedido));
+      if(this.right && this.right.depthFirstForEach(cb,pedido));
+      cb(this.root);
+   }
 };
+//
+// nueva.insert(5);
+// nueva.insert(6);
+// nueva.insert(13);
+// console.log(nueva);
+// console.log(nueva.contains());
+// console.log(nueva.depthFirstForEach("post-order"));
 
 // - breadthFirstForEach: recorre el árbol siguiendo el orden breadth first (BFS)
 
