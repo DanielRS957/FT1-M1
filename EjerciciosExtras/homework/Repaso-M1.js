48fcfbd5fdcacedac67810e0b0032209120b309a
DanielRS957 <drsp9501@icloud.com>
2023-08-11 00:14:15 -0500

    practicando codigo

--- EjerciciosExtras/homework/Repaso-M1.js
+++ EjerciciosExtras/homework/Repaso-M1.js
@@ -9,22 +9,36 @@ const {
 // número u otro array anidado de números, determinar la suma de todos los números contenidos en el array.
 // El array será recibido por parámetro.
 // Ejemplo:
-//    const array = [1, [2, [3,4]], [5,6], 7];
+//    const array1 = [1, [2, [3,4]], [5,6], 7];
 //    countArray(array); --> Debería devolver 28 (1 + 2 + 3 + 4 + 5 + 6 + 7)
 // Pista: utilizar el método Array.isArray() para determinar si algun elemento de array es un array anidado
 // [Para más información del método: https://developer.mozilla.org/es/docs/Web/JavaScript/Referencia/Objetos_globales/Array/isArray]
 
-var countArray = function(array) {
+var countArray = function (array) {
     // Tu código aca:
-    
-}
+    // let suma = 0;
+
+    // for (let i = 0; i < array.length; i++) {
+    //     if (!Array.isArray(array[i])) {
+    //         suma += array[i];
+    //     } else {
+    //         suma += countArray(array[i]);
+    //     }
+    // }
+    // return suma;
+    return array.reduce(
+        (total, element) =>
+            (total += Array.isArray(element) ? countArray(element) : element)
+    );
+};
+// console.log(countArray(array1));
 
 
 // Implementar la función countProps: a partir de un objeto en el cual cada propiedad puede contener
 // cualquier tipo de dato, determinar la cantidad de propiedades de objetos en cualquier nivel, ya sea el inicial
 // u objetos anidados
 // Ejemplo:
-// var obj = {
+// var objeto = {
 //   a: {
 //     a1: 10,
 //     a2: 'Franco',
@@ -33,14 +47,25 @@ var countArray = function(array) {
 //   b: 2,
 //   c: [1, {a: 1}, 'Franco']
 // }
+// }
 // countProps(obj)--> Deberia devolver 10 ya que el objeto inicial tiene 3 propiedades, pero a su vez
 // dentro de a tenemos 3 propiedades mas, luego a3 tiene otras 3 y por ultimo c tiene una extra.
 // Propiedades: a, a1, a2, a3, f, a, c, o, b, c --> 10 en total
 
-var countProps = function(obj) {
+var countProps = function (obj) {
     // Tu código aca:
+    let total = Object.keys(obj).length;
 
+
+    for (let prop in obj) {
+        if (typeof obj[prop] === "object" && !Array.isArray(obj[prop])) {
+            total += countProps(obj[prop]);
+        }
+    }
+
+    return total;
 }
+// countProps(objeto)
 
 
 // Implementar el método changeNotNumbers dentro del prototype de LinkedList que deberá cambiar
@@ -51,10 +76,20 @@ var countProps = function(obj) {
 //    lista.changeNotNumbers();
 //    Ahora la lista quedaría: Head --> [1] --> ['2'] --> [false] --> ['Kirikocho] y la función debería haber devuelto el valor 1
 
-LinkedList.prototype.changeNotNumbers = function(){
+LinkedList.prototype.changeNotNumbers = function () {
     // Tu código aca:
+    let current = this.head;
+    count = 0;
 
-}
+    while (current) {
+        if(isNaN(Nñumber(current.value))) {
+            current.value = "Kiricocho";
+            count++;
+        }
+        current = current.next;
+    }
+    return count;
+};
 
 
 // Implementar la función mergeQueues que a partir de dos queues recibidas por parametro
@@ -65,10 +100,16 @@ LinkedList.prototype.changeNotNumbers = function(){
 // mergeQueues(queueOne, queueTwo) --> [7,2,3,4,5,6]
 // IMPORTANTE: NO son arreglos sino que son Queues.
 
-var mergeQueues = function(queueOne, queueTwo) {
+var mergeQueues = function (queueOne, queueTwo) {
     // Tu código aca:
+    let merged = new Queue();
 
-}
+    while (queueOne.size() || queueTwo.size()) {
+        if (queueOne.size()) merged.enqueue(queueOne.dequeue());
+        if (queueTwo.size()) merged.enqueue(queueTwo.dequeue());
+    }
+    return merged;
+};
 
 
 // Implementar la funcion closureMult que permita generar nuevas funciones que representen
@@ -80,15 +121,22 @@ var mergeQueues = function(queueOne, queueTwo) {
 // - var multBySix = closureMult(6);
 // - multBySix(4) --> 24
 
-var closureMult = function(multiplier) {
+var closureMult = function (multiplier) {
     // Tu código aca:
+    return function (num) {
+        return num * multiplier;
+    };
 
 }
 
 // Implementar el método sum dentro del prototype de BinarySearchTree
 // que debe retornar la suma total de los valores dentro de cada nodo del arbol
-BinarySearchTree.prototype.sum = function() {
+BinarySearchTree.prototype.sum = function () {
     // Tu código aca:
+    let suma = this.value;
+    if (this.right) suma += this.right.sum();
+    if (this.left) suma += this.left.sum();
+    return suma;
 
 }
 
